90be2f902998badfacd99d6d8084f6ef75f7021e
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-03-25 09:54:35 +0530

    Two Pointer Concept

---
+++ src/twopointers_theory/Notes.java
@@ -0,0 +1,37 @@
+package twopointers_theory;
+
+public class Notes {
+    public static void main(String[] args) {
+
+        /*
+
+        Two Pointer Concept
+        -------------------
+
+💡 Core Idea
+-------------
+👉 Use two indices instead of nested loops
+👉 Reduce time complexity O(n² → O(n))
+
+
+🔑 When to Use
+---------------
+Sorted array
+Pair problems (sum, difference)
+Remove duplicates
+Palindrome / reverse
+
+🎯 Types of Two Pointer
+------------------------
+
+1. Opposite Direction
+left →     ← right
+
+2. Same Direction (Slow & Fast)
+slow → → →
+fast → → → →
+
+
+         */
+    }
+}
