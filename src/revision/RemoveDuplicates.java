97ead6131774dcf7cd6ac5d73e636fd1ae112d16
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-04-14 18:22:45 +0530

    Remove Duplicates from Sorted Array

---
+++ src/revision/RemoveDuplicates.java
@@ -0,0 +1,33 @@
+package revision;
+
+// Problem: Remove Duplicates from Sorted Array
+// Approach: Two Pointer (Slow & Fast)
+// Time: O(n), Space: O(1)
+
+public class RemoveDuplicates {
+
+    public static int removeDuplicates(int[] nums) {
+
+        if (nums.length == 0) return 0;
+
+        int slow = 0;
+
+        for (int fast = 1; fast < nums.length; fast++) {
+
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+
+        return slow + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 2, 3};
+
+        int length = removeDuplicates(nums);
+
+        System.out.println("Length: " + length);
+    }
+}
