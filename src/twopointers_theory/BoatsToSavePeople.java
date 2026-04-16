4275e01d5745aac7731a23f95e7cb5a34239655f
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-04-16 10:14:00 +0530

    Added Two Pointer - Trapping Rain Water and Boats to Save People (advanced level)

---
+++ src/twopointers_theory/BoatsToSavePeople.java
@@ -0,0 +1,36 @@
+package twopointers_theory;
+
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+
+    public static int numRescueBoats(int[] people, int limit) {
+
+        Arrays.sort(people);
+
+        int left = 0;
+        int right = people.length - 1;
+
+        int boats = 0;
+
+        while (left <= right) {
+
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+
+            right--;
+            boats++;
+        }
+
+        return boats;
+    }
+
+    public static void main(String[] args) {
+
+        int[] people = {3, 2, 2, 1};
+        int limit = 3;
+
+        System.out.println(numRescueBoats(people, limit));  // 3
+    }
+}
