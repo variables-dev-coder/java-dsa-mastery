524961f53ccf847190336b5fe27c86f2a0dd0f2b
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-04-22 17:25:41 +0530

    Added Sliding Window - Max Sum Subarray of size K (LC 643)

--- src/slidingwindow/MaxSumSubarrayK.java
+++ src/slidingwindow/MaxSumSubarrayK.java
@@ -1,5 +1,43 @@
 package slidingwindow;
 
+/*
+Problem: Maximum Sum Subarray of Size K
+---------------------------------------
+
+Maximum Average Subarray
+ -----------------------I
+
+Why This Problem?
+----------------
+
+This is the foundation of sliding window
+If you understand this → whole pattern becomes easy
+
+Problem Understanding
+
+Given:
+
+Array
+
+Window size k
+
+Find maximum sum (or average) of subarray of size k
+
+Brute Force
+
+Check all subarrays
+
+Time = O(n²)
+
+Sliding Window Idea
+
+Instead of recalculating:
+
+Window 1 → sum
+
+Window 2 → reuse previous sum
+
+ */
 
 
 public class MaxSumSubarrayK {
@@ -36,3 +74,20 @@ public class MaxSumSubarrayK {
         System.out.println("Max Sum: " + maxSum(nums, k)); // Output: 9
     }
 }
+
+/*
+Dry Run
+
+Input:
+
+[2, 1, 5, 1, 3, 2], k = 3
+
+Windows:
+
+[2,1,5] = 8
+[1,5,1] = 7
+[5,1,3] = 9  ← max
+[1,3,2] = 6
+
+
+ */
