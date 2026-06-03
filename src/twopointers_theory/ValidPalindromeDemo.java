089657d7e198549bfb9350b24307b2a79fd7cbe3
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-06-03 17:48:29 +0530

    Added, LeetCode 125, Is Palindrome

---
+++ src/twopointers_theory/ValidPalindromeDemo.java
@@ -0,0 +1,36 @@
+package twopointers_theory;
+
+public class ValidPalindromeDemo {
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal:Panama";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            char l = Character.toLowerCase(s.charAt(left));
+
+            char r = Character.toLowerCase(s.charAt(right));
+
+            if (l != r) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
