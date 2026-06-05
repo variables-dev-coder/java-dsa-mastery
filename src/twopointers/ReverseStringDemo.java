7c385dc150c8b92776a798210a2cfe6f5f488691
Munna Aziz Mondal <azizmunna661@gmail.com>
2026-06-05 17:19:46 +0530

    Added LeetCode 344, Reverse String

---
+++ src/twopointers/ReverseStringDemo.java
@@ -0,0 +1,31 @@
+package twopointers;
+
+import java.util.Arrays;
+
+public class ReverseStringDemo {
+
+    public static void reverseString(char[] s) {
+
+        int left = 0;
+        int right = s.length - 1;
+
+        while(left < right) {
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        char[] s = {'h','e','l','l','o'};
+
+        reverseString(s);
+
+        System.out.println(Arrays.toString(s));
+    }
+}
